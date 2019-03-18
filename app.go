37955e25bdda0b24e862d88450f272ffa09da458
Nineleaps <you@example.com>
2019-03-18 09:39:39 +0530

    Removed println statement

--- app.go
+++ app.go
@@ -40,7 +40,6 @@ func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	var t TrendingArticles
 	resp, _ := http.Get("https://newsapi.org/v2/top-headlines?country=us&apiKey=5b99e9a609584c1fb1f13793c084f410")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(bytes))
 	json.Unmarshal(bytes, &t)
 	p := ArticleAggPage{Title: "Trending Articles on Run", Articles: t}
 	ex, _ := template.ParseFiles("homePage.html")
